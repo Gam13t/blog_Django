583af15b47d3c22467179d86bc7f2bf14444844d
RostislavStarostenko <53529326+RostislavStarostenko@users.noreply.github.com>
2019-08-15 03:07:53 +0300

    Sorry
    I'm so baaaad))0

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -10,3 +11,26 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    first_name = forms.RegexField(regex=r'^[\w.@+-]+$',
+                                  max_length=30,
+                                  label=("First name"),
+                                  error_messages={'invalid': (
+                                      "This value may contain only letters, numbers and @/./+/-/_ characters.")})
+    last_name = forms.RegexField(regex=r'^[\w.@+-]+$',
+                                 max_length=30,
+                                 label=("Last name"),
+                                 error_messages={'invalid': (
+                                     "This value may contain only letters, numbers and @/./+/-/_ characters.")})
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from django.core.mail import send_mail
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 def register(request):
@@ -11,11 +12,40 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Your account has been created! You are able to log in!')
+            send_mail("Greetings, my new blogger",
+
+                      "You're the new user of our blog, {}! If you have any questions - ask out root!"
+                      " Post everything you want! Hope you will like our resource! GL, HF <3!!!".format(username),
+
+                      'postforgithub@gmail.com',
+
+                      [form.cleaned_data.get('email')]
+                      )
             return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
+
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been successfully updated!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form,
+    }
+
+    return render(request, 'users/profile.html', context)
